e7565069009a80845fb34b53ef78c7af76e3123c
Michal Szewczak <michaelszewczak@gmail.com>
2016-11-26 22:55:17 +0000

    smtp works now

--- smtp.py
+++ smtp.py
@@ -1,8 +1,19 @@
 import smtplib
+from email.mime.text import MIMEText
 
-server = smtplib.SMTP('smtp.gmail.com:587')
+class GmailSender:
+    
+    def __init__(self, login, password):
+        self.client = smtplib.SMTP('smtp.gmail.com:587')
+        self.login = login
 
-server.starttls()
-server.sendmail('devremoterm@gmail.com', 'darian.sastre@gmail.com', 'Test')
+        self.client.starttls()
+        self.client.login(self.login, password)
 
-server.close()
+    def send_mail(self, mail):
+        mime_text = MIMEText(mail['body'])
+        mime_text['Subject'] = mail['subject']
+        mime_text['From'] = self.login
+        mime_text['To'] = mail['to']
+        self.client.sendmail(self.login, mime_text['To'], str(mime_text))
+
