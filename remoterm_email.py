7a08e57fc686c009aba1ddc62cf4023fdb24d1a6
darian <darian.sastre@gmail.com>
2016-11-27 10:21:42 +0000

    Fix main

--- remoterm_email.py
+++ remoterm_email.py
@@ -2,6 +2,7 @@ import time
 import imap
 import smtp
 import config
+import IO
 
 def parse_email(body):
     separator = body.find('\n')
@@ -9,7 +10,8 @@ def parse_email(body):
     if separator == -1:
         return None, None
     else:
-        return body[:separator-1], body[separator+1:]
+        separator2 = body.find('\n', separator+1)
+        return body[:separator-1], body[separator+1:separator2-1]
 
 g_receiver = imap.GmailGetter(config.EMAIL, config.EMAIL_PASSWORD)
 
@@ -21,11 +23,18 @@ while (True):
         to = email['sender']
         subject = 'Re: ' + email['subject']
         password, command = parse_email(email['body'])
+        print(command, len(command))
 
         if password == config.APP_PASSWORD:
             print('PASSWORD MATCH')
-            body = command
-            new_mail = {'to' : to, 'subject' : subject, 'body' : body}
+
+            shell = IO.ShellManager('/bin/bash')
+            output = shell.getOutput(command)
+            shell.exitProgram()
+
+            print(output)
+
+            new_mail = {'to' : to, 'subject' : subject, 'body' : output}
 
             g_sender = smtp.GmailSender(config.EMAIL, config.EMAIL_PASSWORD)
             g_sender.send_mail(new_mail)
